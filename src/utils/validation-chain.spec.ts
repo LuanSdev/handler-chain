3b53eada1c4507b9b73444cc1d90aaf131598659
indominuShot <luang193@gmail.com>
2022-03-05 23:19:16 -0300

    feat: ensure sut calls provided method

--- src/utils/validation-chain.spec.ts
+++ src/utils/validation-chain.spec.ts
@@ -4,6 +4,7 @@ class ValidationChain {
   protected data: any;
 
   async execute(method: () => Promise<void>): Promise<ChainValidation> {
+    await method();
     return new ChainValidation();
   }
 }
@@ -20,5 +21,25 @@ describe('Validation chain', () => {
 
     const response = await sut.execute(async () => null);
     expect(response instanceof ChainValidation).toBe(true);
+  });
+
+  it('Should calls provided method', async () => {
+    const { sut } = makeSut();
+
+    class SomeClass {
+      public callsCount: number;
+      constructor() {
+        this.callsCount = 0;
+      }
+
+      async someMethod() {
+        this.callsCount++;
+      }
+    }
+
+    const someClass = new SomeClass();
+
+    await sut.execute(() => someClass.someMethod());
+    expect(someClass.callsCount).toBe(1);
   });
 });
